d0c9db342ae5b8c23c3c0052815c15b308429be5
sashashreyder <sashashreiderbr@gmail.com>
2025-01-10 03:03:00 -0300

    Media Q updated

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,49 +6,47 @@ function HeroSection() {
 
   return (
     <div className='HeroSection'>
-      <div className='text-container'>
-        <h1
-          className='greeting'
-          onMouseEnter={() => setShowGif(true)}
-          onMouseLeave={() => setShowGif(false)}
-        >
-          Hello
-        </h1>
-        <hr className="line" />
-        <p className='description'>
-          I’m Sasha, a junior frontend developer who loves coding and learning new things every day.
-          I’m also into design, always looking for fresh ideas and keeping up with the latest trends.
-          I work well with others and believe good communication is key to any great project.
-        </p>
-      </div>
-
-      <div className="skills-icons">
-        <img src="src/media/HTML5_sh.jpg" alt="HTML" />
-        <img src="src/media/CSS3.jpg" alt="CSS" />
-        <img src="src/media/JS.jpg" alt="JS" />
-        <img src="src/media/react.jpg" alt="React" />
-      </div>
-
-      <div className="image-container">
-        <img id='profilePic' src='https://i.pinimg.com/736x/b1/cc/99/b1cc9987043f82eda1963ab8ba5d03c5.jpg' alt="Profile" />
-        <img id='codePic' src='/src/media/code.png' alt="Code Background" />
-      </div>
-
-      {/* Conditionally render GIF */}
-      {showGif && (
-        <div className="gif-container">
-          <img
-            src="https://i.gifer.com/Ao.gif"
-            alt="Greeting GIF"
-            className="greeting-gif"
-          />
+      <div className="glass-container">
+        <div className='text-container'>
+          <h1
+            className='greeting'
+            onMouseEnter={() => setShowGif(true)}
+            onMouseLeave={() => setShowGif(false)}
+          >
+            Hello
+          </h1>
+          <hr className="line" />
+          <p className='description'>
+            I’m Sasha, a junior frontend developer who loves coding and learning new things every day.
+            I’m also into design, always looking for fresh ideas and keeping up with the latest trends.
+            I work well with others and believe good communication is key to any great project.
+          </p>
         </div>
-      )}
+
+        <div className="skills-icons">
+          <img src="src/media/HTML5_sh.jpg" alt="HTML" />
+          <img src="src/media/CSS3.jpg" alt="CSS" />
+          <img src="src/media/JS.jpg" alt="JS" />
+          <img src="src/media/react.jpg" alt="React" />
+        </div>
+
+        <div className="image-container">
+          <img id='profilePic' src='https://i.pinimg.com/736x/b1/cc/99/b1cc9987043f82eda1963ab8ba5d03c5.jpg' alt="Profile" />
+          <img id='codePic' src='/src/media/code.png' alt="Code Background" />
+        </div>
+
+        {showGif && (
+          <div className="gif-container">
+            <img
+              src="https://i.gifer.com/Ao.gif"
+              alt="Greeting GIF"
+              className="greeting-gif"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 export default HeroSection;
-
-
-
