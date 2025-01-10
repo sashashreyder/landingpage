ee3f188ba056100ed7dbdd3e52c8ea10e3267eb0
sashashreyder <sashashreiderbr@gmail.com>
2025-01-10 15:00:49 -0300

    Left-side Navbar added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,23 @@
-import './Navbar.css'
+import React from 'react';
+import './Navbar.css';
 
 function Navbar() {
     return (
-        <div className='Navbar'>
-            <div className='myName'>
-                <h3>Aleksandra</h3>
-            </div>
+        <div className="navbar-column">
+            <a href="#about" className="nav-icon">
+  <img
+    src="https://i.postimg.cc/W4Qq95wM/cruelty-free-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png"
+    alt="About"
+  />
+</a>
+            <a href="#projects" className="nav-icon">
+                <img src="https://i.postimg.cc/rpWwpM5r/developer-mode-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png" alt="Projects" />
+            </a>
+            <a href="#contact" className="nav-icon">
+                <img src="https://i.postimg.cc/RCP44N2Y/mail-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png" alt="Contact" />
+            </a>
         </div>
-        
     );
 }
 
-export default Navbar; 
+export default Navbar;
