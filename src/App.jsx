9a731711460bd4d1f17e7f0f1c931d7e86920fdb
sashashreyder <sashashreiderbr@gmail.com>
2025-01-10 00:37:21 -0300

    deleted Header Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Header'
 import HeroSection from './HeroSection'
 
 function App() {
 
   return (
     <>
-    <Header/>
     <HeroSection/>
     </> 
   )
