50e2d76a127c71263937a2b3b5e10b4a7f33cedc
sashashreyder <sashashreiderbr@gmail.com>
2025-01-17 00:45:38 -0300

    Content Boxes added in about me section

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -3,10 +3,50 @@ import './AboutMe.css';
 
 function AboutMe() {
     return (
-        <div className='AboutMe'>
-            <div className='text-container-aboutme'>
-                <div className="handwriting-container">
-                    <span className="handwriting-text">Let's get acquainted!</span>
+        <div className="AboutMe">
+            <div className="background-container"></div>
+                <div className="text-container-aboutme">
+                    <div className="handwriting-container">
+                        <span className="handwriting-text">Let's get acquainted!</span>
+                </div>
+            </div>
+
+            {/* Content Section */}
+            <div className="content-boxes">
+                {/* Box 1 */}
+                <div className="content-box">
+                    <div className="content-image image-left"></div>
+                    <div className="content-text">
+                        <h3>Your Title Here</h3>
+                        <p>Your description or story here for this timeline step.</p>
+                    </div>
+                </div>
+
+                {/* Box 2 */}
+                <div className="content-box reverse">
+                    <div className="content-text">
+                        <h3>Your Title Here</h3>
+                        <p>Your description or story here for this timeline step.</p>
+                    </div>
+                    <div className="content-image image-right"></div>
+                </div>
+
+                {/* Box 3 */}
+                <div className="content-box">
+                    <div className="content-image image-left"></div>
+                    <div className="content-text">
+                        <h3>Your Title Here</h3>
+                        <p>Your description or story here for this timeline step.</p>
+                    </div>
+                </div>
+
+                {/* Box 4 */}
+                <div className="content-box reverse">
+                    <div className="content-text">
+                        <h3>Your Title Here</h3>
+                        <p>Your description or story here for this timeline step.</p>
+                    </div>
+                    <div className="content-image image-right"></div>
                 </div>
             </div>
         </div>
@@ -14,4 +54,5 @@ function AboutMe() {
 }
 
 export default AboutMe;
+
 
