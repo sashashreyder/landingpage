40be3dfc6db31616c25b117efd26b075cc15492a
sashashreyder <sashashreiderbr@gmail.com>
2025-01-31 11:04:40 -0300

    assets tranfered to public, pics work

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -13,18 +13,18 @@ function HeroSection() {
   };
 
   return (
-    <div id='HeroSection'>
+    <div id="HeroSection">
       <div className="glass-container">
-        <div className='text-container'>
+        <div className="text-container">
           <h1
-            className='greeting'
+            className="greeting"
             onMouseEnter={() => setShowGif(true)}
             onMouseLeave={() => setShowGif(false)}
           >
             Hello
           </h1>
           <hr className="line" />
-          <p className='description'>
+          <p className="description">
             I’m Sasha, a frontend developer who loves coding and learning new things every day.
             I’m also into design, always looking for fresh ideas and keeping up with the latest trends.
             I work well with others and believe good communication is key to any great project.
@@ -32,16 +32,16 @@ function HeroSection() {
         </div>
 
         <div className="skills-icons">
-          <img src="src/assets/HTML5-sh.jpg" alt="HTML" />
-          <img src="src/assets/CSS3.jpg" alt="CSS" />
-          <img src="src/assets/JS.jpg" alt="JS" />
-          <img src="src/assets/react.jpg" alt="React" />
+          <img src="/landingpage/HTML5-sh.jpg" alt="HTML" />
+          <img src="/landingpage/CSS3.jpg" alt="CSS" />
+          <img src="/landingpage/JS.jpg" alt="JS" />
+          <img src="/landingpage/react.jpg" alt="React" />
         </div>
 
         <div className="image-container">
           <img
-            id='profilePic'
-            src='https://i.pinimg.com/736x/b1/cc/99/b1cc9987043f82eda1963ab8ba5d03c5.jpg'
+            id="profilePic"
+            src="https://i.pinimg.com/736x/b1/cc/99/b1cc9987043f82eda1963ab8ba5d03c5.jpg"
             alt="Profile"
             onMouseEnter={() => setShowOtherGif(true)}
             onMouseLeave={() => setShowOtherGif(false)}
@@ -61,7 +61,7 @@ function HeroSection() {
         {showOtherGif && (
           <div className="other-gif-container">
             <img
-              src="https://i.gifer.com/Idaz.gif" 
+              src="https://i.gifer.com/Idaz.gif"
               alt="Profile GIF"
               className="profile-gif"
             />
@@ -73,4 +73,7 @@ function HeroSection() {
 }
 
 export default HeroSection;
+
+
+
 
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
     return (
         <div className="navbar-column">
             <a href="#HeroSection" className="nav-icon">
-              <img src="src/assets/cruelty_free_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48 (1).png" alt="About" />
+                <img src="/landingpage/public/cruelty_free_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48 (1).png" alt="About" />
             </a>
             <a href="#Portfolio" className="nav-icon">
-                <img src="src/assets/mail-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png" alt="Projects" />
+                <img src="/landingpage/public/developer_mode_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48.png" alt="Projects" />
             </a>
             <a href="#Contact" className="nav-icon">
                 <img src="https://i.postimg.cc/RCP44N2Y/mail-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png" alt="Contact" />
@@ -18,3 +18,6 @@ function Navbar() {
 }
 
 export default Navbar;
+
+
+
