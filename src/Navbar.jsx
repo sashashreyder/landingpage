61f4b775b654564b8b809fbbb8ffc387eae00bdc
sashashreyder <sashashreiderbr@gmail.com>
2025-01-31 11:13:59 -0300

    Fixed image paths for Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,13 +5,13 @@ function Navbar() {
     return (
         <div className="navbar-column">
             <a href="#HeroSection" className="nav-icon">
-                <img src="/landingpage/public/cruelty_free_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48 (1).png" alt="About" />
+                <img src="/landingpage/cruelty_free_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48 (1).png" alt="About" />
             </a>
             <a href="#Portfolio" className="nav-icon">
                 <img src="/landingpage/developer_mode_60dp_FFFFFF_FILL0_wght400_GRAD0_opsz48.png" alt="Projects" />
             </a>
             <a href="#Contact" className="nav-icon">
-                <img src="https://i.postimg.cc/RCP44N2Y/mail-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png" alt="Contact" />
+                <img src="/landingpage/mail-60dp-FFFFFF-FILL0-wght400-GRAD0-opsz48.png" alt="Contact" />
             </a>
         </div>
     );
